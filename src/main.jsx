ba2aec8f61db449e359ba43b05ef79f0c349b119
Anurag Mishra <doitmywayweb@gmail.com>
2025-08-14 04:30:32 +0530

    refactor: clean up main.jsx by removing unused imports and maintaining consistent import style

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
